12412eebf038a893b152e94cf379178c6461b941
ragu <ragu25@codingtown.com>
2018-07-09 17:24:54 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Container, Header, Content, Button, Text } from "native-base";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Button light>
+            <Text> Light </Text>
+          </Button>
+          <Button primary>
+            <Text> Primary </Text>
+          </Button>
+          <Button success>
+            <Text> Success </Text>
+          </Button>
+          <Button info>
+            <Text> Info </Text>
+          </Button>
+          <Button warning>
+            <Text> Warning </Text>
+          </Button>
+          <Button danger>
+            <Text> Danger </Text>
+          </Button>
+          <Button dark>
+            <Text> Dark </Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
+}
